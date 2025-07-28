5c5db3058522a48298a05b7d456c700e90000d55
Bruna de Andrade Braguin <62024853+BrunaABraguin@users.noreply.github.com>
2025-07-28 16:34:35 -0300

    build: Add rewrites api dash/transactions

--- apps/transactions/next.config.ts
+++ apps/transactions/next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
