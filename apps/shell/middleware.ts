13fb31b3ce32c3114c04716107ee3ba79b843303
Bruna de Andrade Braguin <62024853+BrunaABraguin@users.noreply.github.com>
2025-07-28 15:03:56 -0300

    refactor: Redirect shell when has auth

--- apps/shell/middleware.ts
+++ apps/shell/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  if (!req.cookies.get("auth_token")) {
+  if (req.cookies.get("auth_token")) {
     return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
   }
 
