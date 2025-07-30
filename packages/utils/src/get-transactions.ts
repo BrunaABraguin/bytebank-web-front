ee95925c42c515fb23a0682186aa9faba8037883
Bruna de Andrade Braguin <62024853+BrunaABraguin@users.noreply.github.com>
2025-07-29 22:06:50 -0300

    build: Update get transaction

--- packages/utils/src/get-transactions.ts
+++ packages/utils/src/get-transactions.ts
@@ -1,6 +1,6 @@
 import { createHttpService } from "@bytebank-web/utils/http";
-import { Transaction } from "@bytebank-web/types/transaction";
 import { API_URL } from "./contants.js";
+import { Transaction } from "@bytebank-web/types/transaction";
 
 export const getTransactions = async (
   ownerEmail: string | null,
